3ec0b40f8c5fd833218067be4eae6c5d9a0e0dab
Mihail Ivanchev <mihail12@gmail.com>
2012-12-23 23:29:00 +0100

    Build scripts updated: rsync causing errorlevel bugs.

--- haxelib/templates/platforms/run-linux-x86.sh
+++ haxelib/templates/platforms/run-linux-x86.sh
@@ -23,11 +23,11 @@ if [ "$1" == "release" ]; then arg=; fi
 config= ../../../game.config
 
 cd platforms/linux-x86/bin || error "failed to change to platform directory"
-rsync -rlpuq --delete ../../../res/ res || error-cd "failed to copy resource files"
 if [ -f $config ]; then
     cp $config game.config || error-cd "failed to copy application config"
 else
     rm -f $config || error-cd "failed to remove out-dated application config"
 fi
+rsync -rlpuq --delete ../../../res/ res || error-cd "failed to copy resource files"
 ./game$suffix $arg $2 $3 $4 $5 $6 $7 $8 $9 || error-cd "application execution was unsuccessful"
 cd ../../..
--- haxelib/templates/platforms/run-windows-x64.bat
+++ haxelib/templates/platforms/run-windows-x64.bat
@@ -9,14 +9,14 @@
 cd platforms\windows-x64\bin
 @ IF errorlevel 1 call :error "failed to change to platform directory" & exit /B 1
 
-robocopy /mir /njh /njs /ndl /ns /np ..\..\..\res res
-@ IF errorlevel 8 call :error-cd "failed to copy resource files" & exit /B 1
-
 IF NOT EXIST %config% del /q game.config
-@ IF errorlevel 1 call :error-cd "failed to delete out-dated game config"
+@ IF errorlevel 1 call :error-cd "failed to delete out-dated application config" & exit /B 1
 
 IF EXIST %config% copy /y %config% game.config
 @ IF errorlevel 1 call :error-cd "failed to copy application config" & exit /B 1
+
+robocopy /mir /njh /njs /ndl /ns /np ..\..\..\res res
+@ IF errorlevel 8 call :error-cd "failed to copy resource files" & exit /B 1
 
 game%suffix: =% %arg% %2 %3 %4 %5 %6 %7 %8 %9
 @ IF errorlevel 1 call :error-cd "application execution was unsuccessful" & exit /B 1
